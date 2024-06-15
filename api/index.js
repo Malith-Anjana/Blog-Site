788c837ce9518513fff0f15fb8b852af622a490e
Malith Anjana <malithanjana97@gmail.com>
2024-06-15 12:26:38 +0530

    Build api routes

--- api/index.js
+++ api/index.js
@@ -1,8 +1,13 @@
 import express from "express"
-
+import postRoutes from "./routes/post.js"
+import userRoutes from "./routes/post.js"
+import authRoutes from "./routes/post.js"
 const app = express();
 
 app.use(express.json());
+app.use('/api/posts', postRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/auth', authRoutes)
 
 app.listen(8800, ()=> {
     console.log("Connected!")
